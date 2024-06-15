5f5c73536bbcc45801cd12e9b182b96f4241375b
Yiğit Doğan <yigitdogan2k@gmail.com>
2024-06-16 00:21:08 +0300

    Create basics-2.cpp

---
+++ basics-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void) {
+
+	char c = 65;
+	std::cout << c << std::endl;
+	//it prints 'A'
+
+	char d = 65;
+	char e = 10;
+	std::cout << d + e << std::endl;
+	//c++ understands that we're trying to sum
+
+	return 0;
+}
