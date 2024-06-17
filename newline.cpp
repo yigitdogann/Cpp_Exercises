29859b11f5294131dd030dc1f5b325e05f8ee4ef
Yiğit Doğan <yigitdogan2k@gmail.com>
2024-06-17 23:24:15 +0300

    Create newline.cpp

---
+++ newline.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void) {
+	char myVariable = 'X';
+
+	std::cout << "hi there" << '\n';
+	std::cout << 123 << '\n';
+	std::cout << myVariable << std::endl;
+	std::cout << "just some exercises for understanding newline";
+
+	return 0;
+}
