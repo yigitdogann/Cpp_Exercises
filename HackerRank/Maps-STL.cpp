3a6dc571491c304e736b4606e5f68f806256fb85
Yiğit Doğan <yigitdogan2k@gmail.com>
2024-07-09 12:40:13 +0300

    Create Maps-STL.cpp

---
+++ HackerRank/Maps-STL.cpp
@@ -0,0 +1,43 @@
+#include cmath
+#include cstdio
+#include vector
+#include iostream
+#include set
+#include map
+#include algorithm
+using namespace std;
+
+
+int main() {
+     Enter your code here. Read input from STDIN. Print output to STDOUT    
+    int q;  cinq;
+    
+    mapstring, int student;
+    
+    for(int i=0; iq; i++){
+        int x, y;   string name;
+        cinxname;
+        
+        switch (x) {
+            case 1
+                cin  y;
+                student.insert(make_pair(name, y));
+                student[name] += y;
+                break;
+            case 2
+                student.erase(name);
+                break;
+            case 3
+                coutstudent[name]endl;
+                break;
+            default
+                break;
+        }
+    }
+    
+    
+    return 0;
+}
+
+
+
